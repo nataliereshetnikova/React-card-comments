512d6ca865a98a9a2a9a606f3e5bd15e48b493a1
Nataliia Reshetnikova <nataliiareshetnikova@Nataliias-MacBook-Pro.local>
2020-09-14 14:02:21 -0400

    Updates

---
+++ precache-manifest.6482e858d44b304b3175f9c12b1df113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ed795af523ff6f80b3ebf21eb2b6f6",
+    "url": "/React-card-comments/index.html"
+  },
+  {
+    "revision": "a0a3d6cfb50d25647902",
+    "url": "/React-card-comments/static/js/2.83934e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-card-comments/static/js/2.83934e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9f16bbbcad7c5170d9",
+    "url": "/React-card-comments/static/js/main.6d0cc66d.chunk.js"
+  },
+  {
+    "revision": "e6679fcaa93b9b6c1191",
+    "url": "/React-card-comments/static/js/runtime-main.75c6fe4a.js"
+  }
+]);
